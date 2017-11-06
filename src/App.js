c164b4391fb038010298e87989a726d73fd7a976
dncllng <aden.collinge@gmail.com>
2017-11-05 21:14:01 -0500

    First commit

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+<
+            div >
+            <p className ='statement'> BIENVENUE À < span className="visib" > VISIBLE.EXPOSED < /span> <br / >
+            VISIBLE EST UN COLLECTIF D’ ART QUI S’INTÉRESSE < br / >
+            À LA POSTURE DE L’INDIVIDU DANS LA SOCIÉTÉ. < br / >
+            UN PROCESSUS DOCUMENTAIRE EST À L’ORIGINE < br / >
+            DE TOUTES LEURS OEUVRES. < br / >
+            <span className='visib'>VISIBLE</span> EST ISSU DE LA RENCONTRE ENTRE < br / >
+            L’ARTISTE VISUELLE VERONICA MOCKLER < br / >
+            ET DE L’ARTISTE DE THÉATRE SOFIA BLONDIN. < br / >
+                <br / >
+            WELCOME TO <span className='visib'>VISIBLE.EXPOSED </span>< br / >
+            <span className='visib'>VISIBLE</span> IS AN ART COLLECTIVE THAT FOCUSES < br / >
+            ON THE INDIVIDUAL'S POSITION WITHIN SOCIETY.<br/>
+            DOCUMENTARY PROCESSES ARE AT THE BASIS OF THEIR WORK. < br / >
+            <span className='visib'>VISIBLE</span> IS A COLLABORATION BETWEEN < br / >
+            VISUAL ARTIST VERONICA MOCKLER < br / >
+            AND THEATRE ARTIST SOFIA BLONDIN. < br / >
+            </p> 
+            </div>
+    );
+  }
+}
+
+export default App;
